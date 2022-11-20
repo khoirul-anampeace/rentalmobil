acf1323e7a0d12f6765e267f8a0d3020153d499a
Khoirul Anam <khoirul130203@gmail.com>
2022-11-20 12:45:11 +0700

    add main

--- daftar.php
+++ daftar.php
@@ -17,8 +17,8 @@
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container navsmooth">
                 <a class="navbar-brand" href="index.php">
-
-                    <img src="assets/images/logomerpati.png" alt="">
+                    <!-- <img src="assets/images/logomerpati.png" alt=""> -->
+                    <p>Khoirul Anam</p>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -26,7 +26,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link link-black-daftar" href="index.php#about">About</a>
+                            <a class="nav-link link-black-daftar" href="index.php#about">Log Out</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link link-black-daftar" href="index.php#gallery">Gallery</a>
--- main.php
+++ main.php
@@ -14,33 +14,24 @@
 <body>
     <!-- Navigasi -->
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-light" id="main">
             <div class="container navsmooth">
                 <a class="navbar-brand" href="index.html">
-
                     <img src="assets/images/logomerpati.png" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link link-black-daftar" href="index.html#about">About</a>
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="assets/images/users/avatar-2.jpg" width="35px" height="35px" style="border-radius: 50px;">
+
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link link-black-daftar" href="index.html#gallery">Gallery</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link link-black-daftar" href="index.html#contact">Contact</a>
-                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                    <!-- <div class="d-flex">
-                        <a href="daftar.html" class="btn navbutton-blue-daftar me-3">daftar</a>
-                        <a href="" class="btn navbutton-blue-daftar">login</a>
-                    </div> -->
                 </div>
             </div>
         </nav>
@@ -56,7 +47,18 @@
         </a>
         <!-- ScrollTOp -->
 
+        <?php
 
+        $page = @$_GET['page'];
+        $hal = "page/$page.php";
+        $dashboard = "page/dashboard.php";
+        if (!empty($page) && file_exists($hal)) {
+            include $hal;
+        } else {
+            include $dashboard;
+        }
+
+        ?>
     </main>
 
 
@@ -81,8 +83,7 @@
                         </li>
                         <li class="ms-3">
                             <a href="http://jti.polije.ac.id/">
-                                <img src="assets/images/profileImage/logo_jti.png"
-                                    style="width: 100px; margin-top:8px;">
+                                <img src="assets/images/profileImage/logo_jti.png" style="width: 100px; margin-top:8px;">
                             </a>
                         </li>
                     </ul>
