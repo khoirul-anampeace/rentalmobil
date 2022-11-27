de79db89291d34375a3a0e5dc5fd6c00cd2d8af6
Khoirul Anam <khoirul130203@gmail.com>
2022-11-28 00:49:48 +0700

    update mobilLepasKunci

--- customer/page/mobilLepasKunci.php
+++ customer/page/mobilLepasKunci.php
@@ -1,17 +1,33 @@
-<section class="content" id="mainMobil">
+<section class="content customer" id="mainMobil">
     <div class="container" id="">
         <h1>Pilih Mobil</h1>
         <div class="row pilihmobil">
-            <div class="col-md-6 col-sm-12 row box mobil">
-                <div class="col-4 img-pilihmobil">
-                    <img src="../assets/images/car/cadilacone.jpg">
-                </div>
-                <div class="col-8  text-pilihmobil">
-                    <h5>Limousine Cadilac One </h5>
-                    <p class="harga">Rp 300.000,</p>
-                    <a href=".?page=detailMobil" class="btn btn-secondary">Detail</a>
-                    <a href=".?page=checkoutLepasKunci" class="btn btn-primary float-end">Book</a>
+            <div class="col-md-6 col-sm-12 row  mobil">
+                <div class="box row">
+                    <div class="col-md-4 img-pilihmobil">
+                        <img src="../assets/images/car/cadilacone.jpg">
+                    </div>
+                    <div class="col-md-8  text-pilihmobil">
+                        <h5>Limousine Cadilac One </h5>
+                        <p class="harga">Rp 300.000,</p>
+                        <a href=".?page=detailMobil" class="btn btn-secondary">Detail</a>
+                        <a href=".?page=checkoutLepasKunci" class="btn btn-primary float-end">Book</a>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-6 col-sm-12 row  mobil">
+                <div class="box row">
+                    <div class="col-md-4 img-pilihmobil">
+                        <img src="../assets/images/car/cadilacone.jpg">
+                    </div>
+                    <div class="col-md-8  text-pilihmobil">
+                        <h5>Limousine Cadilac One </h5>
+                        <p class="harga">Rp 300.000,</p>
+                        <a href=".?page=detailMobil" class="btn btn-secondary">Detail</a>
+                        <a href=".?page=checkoutLepasKunci" class="btn btn-primary float-end">Book</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
 </section>
