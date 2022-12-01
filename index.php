8d57effa711c3ba7f51938fc16947d42fd11b366
Khoirul Anam <khoirul130203@gmail.com>
2022-12-01 09:36:02 +0700

    update index landing page

--- index.php
+++ index.php
@@ -214,7 +214,7 @@
                     <h6>ALAMAT</h6>
                     <p class="address">Jl. Sentot Prawirodirjo X/ 201 Jember Kabupaten Jember Jawa Timur 〒68131
                         Indonesia
-                    </a>
+                        </a>
                     <h6 class="mt-5">Jam Kerja</h6>
                     <div class="row">
                         <p>24 Jam</p>
