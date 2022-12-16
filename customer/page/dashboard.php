87708c9d8d9c1b2a93f42be37e223bb3bcad9866
Khoirul Anam <khoirul130203@gmail.com>
2022-12-17 04:40:51 +0700

    perbaiki dashboard customer

--- customer/page/dashboard.php
+++ customer/page/dashboard.php
@@ -17,19 +17,26 @@
                 </div>
             </a>
         </div>
-        <h1>Histori Transaksi</h1>
-        <div class="row histori">
-            <div class="col-md-6 col-sm-12 row box">
-                <div class="col-4 img-histori">
-                    <img src="../assets/images/car/cadilacone.jpg">
-                </div>
-                <div class="col-8  text-histori">
-                    <h5>Limousine Cadilac One</h5>
-                    <p>NO PP : P 3110 PQR</p>
-                    <p>Status : “Menunggu tim survei ke alamat anda”</p>
-                    <a href="" class="btn btn-danger">BATALKAN</a>
+        <h1>Transaksi Terbaru</h1>
+        <?php
+        $transaksiterbaru = query("SELECT t.*, p.*, m.*, km.*, c.* FROM transaksi t JOIN pembayaran p ON t.id_transaksi = p.id_transaksi JOIN mobil m ON t.id_mobil = m.id_mobil JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil JOIN customer c ON t.id_customer = c.id_customer WHERE t.jenis_transaksi = 'lepas kunci' AND t.status_transaksi = 'Menunggu Konfirmasi' OR t.status_transaksi = 'Survei Lokasi'");
+        $no = 1;
+        foreach ($transaksiterbaru as $row) :
+        ?>
+            <div class="row histori">
+                <div class="col-md-8 col-sm-12 row box">
+                    <div class="col-4 img-histori">
+                        <img src="../assets/images/mobil/<?= $row["depan"] ?>">
+                    </div>
+                    <div class="col-8  text-histori">
+                        <h5><?= $row["merk"] ?></h5>
+                        <p><?= $row["no_plat"] ?></p>
+                        <p><?= rupiah($row["harga"]) ?></p>
+                        <p>Status : “<?= $row["status_transaksi"] ?>”</p>
+                        <!-- <a href="" class="btn btn-danger">BATALKAN</a> -->
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
 </section>
