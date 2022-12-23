1769b69a19a1a4bb890c1bd32891ace8c7203d4d
Khoirul Anam <khoirul130203@gmail.com>
2022-12-23 10:48:55 +0700

    filter pilih mobil (customer)

--- customer/page/pilihMobil.php
+++ customer/page/pilihMobil.php
@@ -2,9 +2,9 @@
     <div class="container" id="">
         <h1>Pilih Mobil</h1>
         <div class="row pilihmobil">
-            <form method="GET" class="jenismobil">
+            <form method="POST" class="jenismobil">
                 <div class="jenismobilpilihan">
-                    <button name="jenissuv" class="btn">
+                    <button name="semua" class="btn">
                         <img src="../assets/images/icon/suv-icon.png" alt="">
                         <h6>Semua Mobil</h6>
                     </button>
@@ -20,19 +20,35 @@
                         <img src="../assets/images/icon/sedan-icon.png" alt="">
                         <h6>Sedan</h6>
                     </button>
+                    <button name="jenispickup" class="btn">
+                        <img src="../assets/images/icon/pickup-icon.png" alt="">
+                        <h6>Pickup</h6>
+                    </button>
                 </div>
             </form>
             <?php
             if (isset($_GET["jenis_sewa"]) || $_GET["jenis_sewa"] == "") {
                 # code...
-
-                $no = 1;
-                $jumlahDataMobil = mysqli_query($connect, "SELECT * FROM mobil m JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil WHERE m.status_mobil = 'Tersedia'");
+                $filter = "";
+                if (isset($_POST["semua"])) {
+                    $filter = "";
+                } else if (isset($_POST["jenissuv"])) {
+                    $filter = "AND jenis LIKE 'SUV'";
+                } else if (isset($_POST["jenismpv"])) {
+                    $filter = "AND jenis LIKE 'MPV'";
+                } else if (isset($_POST["jenissedan"])) {
+                    $filter = "AND jenis LIKE 'Sedan'";
+                } else if (isset($_POST["jenispickup"])) {
+                    $filter = "AND jenis LIKE 'Pickup'";
+                }
+                $jumlahDataMobil = mysqli_query($connect, "SELECT * FROM mobil m JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil WHERE m.status_mobil = 'Tersedia' $filter");
                 // echo "total data " . mysqli_num_rows($jumlahDatatransaksi);
                 if (mysqli_num_rows($jumlahDataMobil) == 0) {
                     echo "<p>Mohon Maaf Mobil Tidak Tersedia</p>";
                 } else {
-                    $mobil = query("SELECT * FROM mobil m JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil WHERE m.status_mobil = 'Tersedia'");
+
+                    $mobil = query("SELECT * FROM mobil m JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil WHERE m.status_mobil = 'Tersedia' $filter");
+
                     foreach ($mobil as $row) :
                         // checkout check
                         if ($_GET["jenis_sewa"] == "lepasKunci") {
