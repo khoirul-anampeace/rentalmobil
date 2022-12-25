6a96cec8a0f0667721110d25bea162a3e9196e56
feldaasfld <feldaamelia25@gmail.com>
2022-12-25 14:55:29 +0700

    edit tambah driver

--- admin/page/tambahDriver.php
+++ admin/page/tambahDriver.php
@@ -4,7 +4,7 @@
         <div class="page-title-box">
             <div class="page-title-right">
             </div>
-            <h4 class="page-title">Tambah Driver</h4>
+            <h4 class="page-title">Tambah Paket Driver</h4>
         </div>
     </div>
 </div>
@@ -39,7 +39,7 @@ $id_driver = $huruf . sprintf("%03s", $urutan);
                 <form method="POST" enctype="multipart/form-data">
                     <div class="row">
                         <div class="col-md-6 mb-3">
-                            <label for="inputNama" class="form-label">Id Driver</label>
+                            <label for="inputNama" class="form-label">ID Driver</label>
                             <input type="text" value=" <?= $id_driver ?>" name="idDriver" class="form-control" id="inputMerk" placeholder="" readonly required autocomplete="off">
                         </div>
                         <div class="col-md-6 mb-3">
