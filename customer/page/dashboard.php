2ac3929168ef39c518f7ac5d5eb1be19a60c527d
Khoirul Anam <khoirul130203@gmail.com>
2022-12-28 23:45:50 +0700

    perbaiki dashboard (customer)

--- customer/page/dashboard.php
+++ customer/page/dashboard.php
@@ -24,18 +24,22 @@ $denganDriver = ".?page=pilihMobil&jenis_sewa=denganDriver";
         <h1>Transaksi Terbaru</h1>
         <?php
         $getIdCustomer = $_SESSION["id_customer"];
-        $transaksiterbaru = query("SELECT t.*, p.*, m.*, km.*, c.* FROM transaksi t JOIN pembayaran p ON t.id_transaksi = p.id_transaksi JOIN mobil m ON t.id_mobil = m.id_mobil JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil JOIN customer c ON t.id_customer = c.id_customer WHERE  t.id_customer = '$getIdCustomer' AND (t.status_transaksi = 'Menunggu Konfirmasi' OR t.status_transaksi = 'Survei Lokasi' OR t.status_transaksi = 'Sedang Dalam Peminjaman') ORDER BY t.id_transaksi DESC");
+        $transaksiterbaru = query("SELECT t.*, p.*, m.*, km.*, c.* FROM transaksi t JOIN pembayaran p ON t.id_transaksi = p.id_transaksi JOIN mobil m ON t.id_mobil = m.id_mobil JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil JOIN customer c ON t.id_customer = c.id_customer WHERE  t.id_customer = '$getIdCustomer' AND (t.status_transaksi = 'Menunggu Konfirmasi' OR t.status_transaksi = 'Telah Dipesan' OR t.status_transaksi = 'Sedang Dalam Peminjaman') ORDER BY t.id_transaksi DESC");
         // echo "baris" . mysqli_affected_rows($connect);
-        $jumlahDatatransaksi = mysqli_query($connect, "SELECT t.*, p.*, m.*, km.*, c.* FROM transaksi t JOIN pembayaran p ON t.id_transaksi = p.id_transaksi JOIN mobil m ON t.id_mobil = m.id_mobil JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil JOIN customer c ON t.id_customer = c.id_customer WHERE  t.id_customer = '$getIdCustomer' AND (t.status_transaksi = 'Menunggu Konfirmasi' OR t.status_transaksi = 'Survei Lokasi' OR t.status_transaksi = 'Sedang Dalam Peminjaman')");
+        $jumlahDatatransaksi = mysqli_query($connect, "SELECT t.*, p.*, m.*, km.*, c.* FROM transaksi t JOIN pembayaran p ON t.id_transaksi = p.id_transaksi JOIN mobil m ON t.id_mobil = m.id_mobil JOIN kondisi_mobil km ON m.id_mobil = km.id_mobil JOIN customer c ON t.id_customer = c.id_customer WHERE  t.id_customer = '$getIdCustomer' AND (t.status_transaksi = 'Menunggu Konfirmasi' OR t.status_transaksi = 'Telah Dipesan' OR t.status_transaksi = 'Sedang Dalam Peminjaman')");
         // echo "total data " . mysqli_num_rows($jumlahDatatransaksi);
         if (mysqli_num_rows($jumlahDatatransaksi) == 0) {
             echo "<p>Belum Ada Transaksi Baru</p>";
         } else {
             foreach ($transaksiterbaru as $row) :
                 $statusSewa = $row['status_transaksi'];
-                if ($statusSewa == "Survei Lokasi") {
+                if ($statusSewa == "Telah Dipesan") {
                     // $statusSewa = "Menunggu konfirmasi";
-                    $statusSewa = "Silahkan menunggu, petugas kami akan melakukan survei ke rumah anda";
+                    if ($row["jenis_transaksi"] == "lepas kunci") {
+                        $statusSewa = "Silahkan tunggu, petugas kami akan melakukan survei ke rumah anda";
+                    } else if ($row["jenis_transaksi"] == "dengan driver") {
+                        $statusSewa = "Silahkan tunggu, transaksi anda sedang diperiksa";
+                    }
                 } else if ($statusSewa == "Menunggu Konfirmasi") {
                     $statusSewa = "Menunggu Konfirmasi";
                 }
